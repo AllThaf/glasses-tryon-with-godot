8a266cc1ba859e85647c760db79719894375c4c4
rapip <rafifgenadratama28@gmail.com>
2025-10-30 17:25:48 +0700

    rearrange

--- godot_project/Scripts/main_menu.gd
+++ godot_project/Scripts/main_menu.gd
@@ -1,11 +1,11 @@
 extends Control
 
-@onready var kacamata1_button: Button = $ButtonContainer/ButtonKacamata1
-@onready var kacamata2_button: Button = $ButtonContainer/ButtonKacamata2
-@onready var kacamata3_button: Button = $ButtonContainer/ButtonKacamata3
-@onready var kacamata4_button: Button = $ButtonContainer/ButtonKacamata4
-@onready var tentangKami_button: Button = $ButtonContainer/ButtonTentangKami
-@onready var quit_button: Button = $ButtonContainer/ButtonQuit
+@onready var kacamata1_button: Button = $BoxContainer/TextureRect/ButtonContainer/ButtonKacamata1
+@onready var kacamata2_button: Button = $BoxContainer/TextureRect/ButtonContainer/ButtonKacamata2
+@onready var kacamata3_button: Button = $BoxContainer/TextureRect/ButtonContainer/ButtonKacamata3
+@onready var kacamata4_button: Button = $BoxContainer/TextureRect/ButtonContainer/ButtonKacamata4
+@onready var tentangKami_button: Button = $BoxContainer/TextureRect/ButtonContainer/ButtonTentangKami
+@onready var quit_button: Button = $BoxContainer/TextureRect/ButtonContainer/ButtonQuit
 
 
 func _on_button_quit_pressed() -> void:
@@ -13,27 +13,27 @@ func _on_button_quit_pressed() -> void:
 
 
 func _on_button_tentang_kami_pressed() -> void:
-	get_tree().change_scene_to_file("res://tentang_kami.tscn")
+	get_tree().change_scene_to_file("res://Scenes/tentang_kami.tscn")
 
 
 func _on_button_kacamata_1_pressed() -> void:
 	send_glasses_choice("godot_project/Glasses/glasses1.png")
-	get_tree().change_scene_to_file("res://webcam_client_udp.tscn")
+	get_tree().change_scene_to_file("res://Scenes/webcam_client_udp.tscn")
 
 
 func _on_button_kacamata_2_pressed() -> void:
 	send_glasses_choice("godot_project/Glasses/glasses2.png")
-	get_tree().change_scene_to_file("res://webcam_client_udp.tscn")
+	get_tree().change_scene_to_file("res://Scenes/webcam_client_udp.tscn")
 
 
 func _on_button_kacamata_3_pressed() -> void:
 	send_glasses_choice("godot_project/Glasses/glasses3.png")
-	get_tree().change_scene_to_file("res://webcam_client_udp.tscn")
+	get_tree().change_scene_to_file("res://Scenes/webcam_client_udp.tscn")
 
 
 func _on_button_kacamata_4_pressed() -> void:
 	send_glasses_choice("godot_project/Glasses/glasses4.png")
-	get_tree().change_scene_to_file("res://webcam_client_udp.tscn")
+	get_tree().change_scene_to_file("res://Scenes/webcam_client_udp.tscn")
 
 func send_glasses_choice(path: String) -> void:
 	# Mengirim pesan UDP singkat ke server lokal pada port 8888
--- godot_project/Scripts/webcam_client_udp.gd
+++ godot_project/Scripts/webcam_client_udp.gd
@@ -53,7 +53,7 @@ func _ready():
 
 func _on_quit_button_pressed():
 	disconnect_from_server()
-	get_tree().change_scene_to_file("res://main_menu.tscn")
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
 
 func _process(delta):
 	if is_connect:
